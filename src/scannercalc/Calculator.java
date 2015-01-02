25edd9e7a24905f6f2ff7647a98aad86d191b52a
ktrzcinski <kristof.trz@gmail.com>
2015-01-02 22:59:10 +0000

    Initial creation of RandomApps repository.

---
+++ src/scannercalc/Calculator.java
@@ -0,0 +1,29 @@
+package scannercalc;
+
+public class Calculator {
+	public final String PLUS = "+", MINUS = "-", MULTIPLY = "*", DIVIDE = "/";
+
+	public double calculate(double a, double b, String operator) {
+		double result = 0;
+
+		switch (operator) {
+		case PLUS:
+			result = a + b;
+			break;
+		case MINUS:
+			result = a - b;
+			break;
+		case MULTIPLY:
+			result = a * b;
+			break;
+		case DIVIDE:
+			result = a / b;
+			break;
+		default:
+			System.out.println("Podany operator nie istnieje!");
+		}
+
+		return result;
+	}
+
+}
