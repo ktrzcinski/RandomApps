4f728b7ecd105518cd765c2acbae595c329bb4a0
ktrzcinski <kristof.trz@gmail.com>
2015-01-03 14:00:24 +0000

    Adding ForLoops excercise.

---
+++ src/loops/ForLoop.java
@@ -0,0 +1,22 @@
+package loops;
+
+import java.util.Scanner;
+
+public class ForLoop {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num, sum = 0;
+
+		System.out.print("Enter the amount of numbers you want to sum: ");
+		num = input.nextInt();
+
+		while(num-- > 0) {
+			System.out.print("Enter your next number: ");
+			sum = sum + (input.nextInt());
+		}
+
+		System.out.println("Sum off all numbers equals " + sum);
+
+	}
+
+}
